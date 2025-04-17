6f428f574bdd9524e57bd5fde78841e9e1261897
hassanpydev <hassangames1998@gmail.com>
2025-04-17 16:22:38 +0300

    first init

--- ShipsAuth/urls.py
+++ ShipsAuth/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import login_view,register_view,logout_view,edit_user_view,delete_user_view,user_list_view
+from .views import activate_user_view, deactivate_user_view, reset_user_password_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -8,6 +9,9 @@ urlpatterns = [
     path('user/edit/<int:user_id>/', edit_user_view, name='edit_user'),
     path('user/delete/<int:user_id>/', delete_user_view, name='delete_user'),
     path('user/list/', user_list_view, name='user_list'),
+    path('user/activate/<int:user_id>/', activate_user_view, name='activate_user'),
+    path('user/deactivate/<int:user_id>/', deactivate_user_view, name='deactivate_user'),
+    path('user/reset-password/<int:user_id>/', reset_user_password_view, name='reset_user_password'),
 ]
 # User management URLs
 
